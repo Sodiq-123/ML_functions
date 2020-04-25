9320e65488688d09dd8b35c97f4170a40b1d9449
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-04-25 23:30:14 +0100

    Modified the file

--- FunctionsInMachineLearning.py
+++ FunctionsInMachineLearning.py
@@ -2,9 +2,12 @@ def weightInitialization(n_features):
     w = np.zeros((1,n_features))
     b = 0
     return w,b
+
+
 def sigmoid_activation(result):
     final_result = 1/(1+np.exp(-result))
     return final_result
+
 
 def model_optimize(w, b, X, Y):
     m = X.shape[0]
@@ -13,7 +16,7 @@ def model_optimize(w, b, X, Y):
     final_result = sigmoid_activation(np.dot(w,X.T)+b)
     Y_T = Y.T
     cost = (-1/m)*(np.sum((Y_T*np.log(final_result)) + ((1-Y_T)*(np.log(1-final_result)))))
-    #
+
     
     #Gradient calculation
     dw = (1/m)*(np.dot(X.T, (final_result-Y.T).T))
@@ -22,28 +25,28 @@ def model_optimize(w, b, X, Y):
     grads = {"dw": dw, "db": db}
     
     return grads, cost
+    
 def model_predict(w, b, X, Y, learning_rate, no_iterations):
     costs = []
     for i in range(no_iterations):
-        #
         grads, cost = model_optimize(w,b,X,Y)
-        #
         dw = grads["dw"]
         db = grads["db"]
+
         #weight update
         w = w - (learning_rate * (dw.T))
         b = b - (learning_rate * db)
-        #
         
         if (i % 100 == 0):
             costs.append(cost)
-            #print("Cost after %i iteration is %f" %(i, cost))
+            print("Cost after %i iteration is %f" %(i, cost))
     
     #final parameters
     coeff = {"w": w, "b": b}
     gradient = {"dw": dw, "db": db}
     
     return coeff, gradient, costs
+
 def predict(final_pred, m):
     y_pred = np.zeros((1,m))
     for i in range(final_pred.shape[1]):
