45345cc915aa0ecc78494b1bf971b6c12d023f6a
Sodiq_dev <57923180+Sodiq-123@users.noreply.github.com>
2020-05-29 22:05:33 +0100

    Update FunctionsInMachineLearning.py

--- FunctionsInMachineLearning.py
+++ FunctionsInMachineLearning.py
@@ -53,3 +53,36 @@ def predict(final_pred, m):
         y_pred[0][i] = 1 if final_pred[0][i] > 0.5
         
     return y_pred
+
+
+# How to customise your model to learn your data with ### Keras
+
+class CustomModel(keras.Model):
+    def train_step(self, data):
+    # Unpack the data. Its structure depends on your model and
+    # on what you pass to `fit()`.
+        x, y = data
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)  # Forward pass
+            # Compute the loss value
+            # (the loss function is configured in `compile()`)
+            loss = self.compiled_loss(y, y_pred,
+                                    regularization_losses=self.losses)
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        # Update metrics (includes the metric that tracks the loss)
+        self.compiled_metrics.update_state(y, y_pred)
+        # Return a dict mapping metric names to current value
+        return {m.name: m.result() for m in self.metrics}
+
+# Construct and compile an instance of CustomModel
+inputs = keras.Input(shape=(32,))
+outputs = keras.layers.Dense(1)(inputs)
+model = CustomModel(inputs, outputs)
+model.compile(optimizer='adam', loss='mse', metrics=[...])
+
+# Just use `fit` as usual
+model.fit(dataset, epochs=3, callbacks=...)
